956cd5668b6db26c20401adab9f8459385fbd742
6o66y <6o66y@users.noreply.github.com>
2020-07-03 06:44:58 +0000

    [Add] models/post.rb / [Making] posts_controller.rb, index.html.erb & new.html.erb

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,8 +1,12 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+  	@post = Post.new
   end
 
   def index
+  	@posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -12,11 +16,24 @@ class Public::PostsController < ApplicationController
   end
 
   def create
+  	@post = Post.new(post_params)
+  	@post.user_id = current_user.id
+  	if @post.save
+  		redirect_to posts_path
+  	else
+  		render :new
+  	end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:body)
   end
 end
