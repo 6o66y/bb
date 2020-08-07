f0ce8813a284771343da409f0d821186e3d306ab
6o66y <6o66y@users.noreply.github.com>
2020-08-07 11:21:57 +0000

    [Add] users/edit.html.erb & users/show.html.erb

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,4 +2,27 @@ class Users::UsersController < ApplicationController
   def index
   	@users = User.all
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user)
+  	else
+  		render :show
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:nickname)
+  end
 end
